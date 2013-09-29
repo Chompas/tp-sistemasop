fd0134e7f2bc97c5c21e24694dc35781a5959905
Maria Ines Parnisari <maineparnisari@gmail.com>
2013-09-29 00:06:22 -0300

    Actualizado

--- Grabar_L.sh
+++ Grabar_L.sh
@@ -2,6 +2,7 @@
 cantidadLineasADejar=5		# Cantidad de lineas luego de truncar
 cantidadParametros=$#
 esModuloInstalador=false	# Fue invocado por el instalador?
+debug=false
 
 # Array asociativo con los tipos posibles de mensajes
 declare -A tiposMensajes
@@ -10,7 +11,8 @@ tiposMensajes=([i]=Info [w]=Warning [e]=Error [se]=Severe)
 # Imprime la forma correcta de uso de este script
 function uso() { 
 	echo "Uso: $0 [-i] comando [-t <i|w|e|se>] mensaje" 1>&2;
-	echo -e "\t-i: El modulo invocante es el instalador del TP"
+	echo "Parámetros opcionales:"
+	echo -e "\t-i: El modulo invocante es el instalador del TP."
 	echo -e "\t-t: Tipo de mensaje a loggear:"
 	for tipo in "${!tiposMensajes[@]}"
 	do
@@ -34,9 +36,12 @@ while getopts "hi" opt; do
     esac
 done
 
-shift $(( OPTIND-1 ))		# $N se convierte en $N-1
+if [ "$OPTIND" != 1 ] ; then
+	shift $(( OPTIND-1 ))		# $2 se convierte en $1
+fi
 comando="$1"
 
+OPTIND=2
 # Hay un parametro opcional "-t", que lleva un argumento (el tipo de mensaje)
 while getopts "t:" opt; do
     case "${opt}" in
@@ -59,44 +64,52 @@ while getopts "t:" opt; do
     esac
 done
 
-if [ "$cantidadParametros" -gt 2 ] ; then
-	shift $(($OPTIND - 2))
+if [ "$OPTIND" != 1 ] ; then
+	shift $(( OPTIND-2 ))		# $2 se convierte en $1
 fi
 mensaje="$2"
 
 # ------- FIN PROCESAMIENTO DE ARGUMENTOS -------- 
 
-echo "esModuloInstalador = "$esModuloInstalador
-echo "comando = "$comando
-echo "tipo mensaje = "$tipoMensaje
-echo "mensaje = "$mensaje
-
-LOGDIR="directorio"
+LOGDIR="dirLogs"
 LOGEXT="log"
-LOGSIZE=500
+LOGSIZE=1000
 nombreArchivoDeLog=${comando}.$LOGEXT
 path=${LOGDIR}/${nombreArchivoDeLog}
+fecha=$(date)
+usuario=$USER
 
-echo "path = " $path
+if $debug ; then
+	echo "esModuloInstalador = "$esModuloInstalador
+	echo "comando = "$comando
+	echo "tipo mensaje = "$tipoMensaje
+	echo "mensaje = "$mensaje
+	echo "path = "$path
+	echo "tamanioArchivo = "$tamanioArchivo
+fi
 
 # Si el archivo no existe
-if [ ! -f path ]; then
+if [ ! -f $path ]; then
 	# Si el directorio no existe
     if [ ! -d $LOGDIR ]; then
 		mkdir -m 775 $LOGDIR # Creamos el directorio
 	fi
 	
-	echo -n "" >> $path # Creamos el archivo
+	# Creamos el archivo con el encabezado
+	echo -e "FECHA \t \t \t USUARIO \t COMANDO \t TIPO \t \t MENSAJE" >> $path
 fi
 
-tamanioArchivo=$(stat -c '%s' Grabar_L.sh)
-echo "tamanioArchivo = " $tamanioArchivo
-
-if [ $tamanioArchivo -ge $LOGSIZE ]; then
-	# Truncamos el archivo
+# Si el archivo es muy grande, lo truncamos
+tamanioArchivo=$(stat -c '%s' $path)
+if [ "$tamanioArchivo" -ge "$LOGSIZE" ]; then
+	echo -e "FECHA \t USUARIO \t COMANDO \t TIPO \t MENSAJE" >> ${path}_tmp
 	tail --lines=$cantidadLineasADejar $path >> ${path}_tmp
 	rm $path
 	mv ${path}_tmp $path
-	echo -n "Log Excedido" >> $path		# El parametro -n es para evitar el fin de linea
+	echo "Log Excedido" >> $path		# El parametro -n es para evitar el fin de linea
 fi
+
+# Finalmente... escribimos en el archivo de log!!!
+echo -e "$fecha \t $usuario \t ${tiposMensajes[$tipoMensaje]} \t $mensaje" >> $path
+
 exit 0
